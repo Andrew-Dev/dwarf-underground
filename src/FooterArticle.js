444f08ec5dcb6c544ce7fb49631bc02672b9155c
Andrew Arpasi <andrew@andrewarpasi.com>
2017-06-13 22:02:52 -0400

    Present footer articles with map and props

--- src/FooterArticle.js
+++ src/FooterArticle.js
@@ -3,7 +3,12 @@ import React, { Component } from 'react';
 class FooterArticle extends Component {
     render() {
         return (
-            
+            <div className="small-6 medium-3 columns other-article">
+                <a href="#">
+                    <img src={this.props.image} alt={this.props.alt} />
+                    <p>{this.props.title}</p>
+                </a>  
+            </div>
         )
     }
 }
