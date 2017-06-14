22e2889756c783d23bf974795b3cd72acabdc482
Andrew Arpasi <andrew@andrewarpasi.com>
2017-06-13 20:55:42 -0400

    Break footer and ad into components

---
+++ src/FooterArticle.js
@@ -0,0 +1,11 @@
+import React, { Component } from 'react';
+
+class FooterArticle extends Component {
+    render() {
+        return (
+            
+        )
+    }
+}
+
+export default FooterArticle
